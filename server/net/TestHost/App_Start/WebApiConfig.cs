4241e37732c9d96efb7e9769cc4e9df7e57ba136
annieio <annieio@outlook.com>
2014-07-21 15:53:14 -0700

    Removed unnecessary files from TestHost

--- server/net/TestHost/App_Start/WebApiConfig.cs
+++ server/net/TestHost/App_Start/WebApiConfig.cs
@@ -2,8 +2,6 @@
 using System.Data.Entity;
 using System.Web.Http;
 using Microsoft.WindowsAzure.Mobile.Service;
-using TestHost.DataObjects;
-using TestHost.Models;
 
 namespace TestHost
 {
@@ -16,31 +14,6 @@ namespace TestHost
 
             // Use this class to set WebAPI configuration options
             HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
-
-            // To display errors in the browser during development, uncomment the following
-            // line. Comment it out again when you deploy your service for production use.
-            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
-            
-            Database.SetInitializer(new jresgen2Initializer());
-        }
-    }
-
-    public class jresgen2Initializer : ClearDatabaseSchemaIfModelChanges<TestHostContext>
-    {
-        protected override void Seed(TestHostContext context)
-        {
-            List<TodoItem> todoItems = new List<TodoItem>
-            {
-                new TodoItem { Id = "1", Text = "First item", Complete = false },
-                new TodoItem { Id = "2", Text = "Second item", Complete = false },
-            };
-
-            foreach (TodoItem todoItem in todoItems)
-            {
-                context.Set<TodoItem>().Add(todoItem);
-            }
-
-            base.Seed(context);
         }
     }
 }
