a7a8d7dbf022edf89e513e9aec2ea12aaeafe400
Hasan Khan <haskhan@microsoft.com>
2014-07-31 16:44:48 -0700

    added npm module folder structure

---
+++ server/node/broker/README.md
@@ -0,0 +1,13 @@
+## resource-broker
+
+A general purpose resource broker for Azure
+
+## Installation
+
+    $ npm install commander
+
+## Get Started
+
+```js
+var broker = require('resource-broker');
+```
---
+++ server/node/broker/lib/broker.js
@@ -0,0 +1,15 @@
+var azure = require('azure');
+
+var Broker = (function() {
+    function Broker() {
+        // constructor
+    }
+
+    Broker.prototype.getResource = function (params) {
+        // get the resource
+    };
+
+    return Broker;
+})();
+
+module.exports = exports = Prompt;
